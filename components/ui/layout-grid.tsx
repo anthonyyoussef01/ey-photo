b13cb0bbd19fa666d4febd93c2426e9f2aa48c22
antho <anthonyyoussef01@gmail.com>
2024-07-29 20:51:04 -0400

    Layout Grid Updates:
    - Fixing the gallery layout hover effect

--- components/ui/layout-grid.tsx
+++ components/ui/layout-grid.tsx
@@ -99,7 +99,7 @@ const ImageComponent = ({ card }: { card: Card }) => {
             height="500"
             width="500"
             className={cn(
-                "object-cover object-top absolute inset-0 h-full w-full transition duration-200",
+                "object-cover object-top absolute inset-0 h-full w-full transition duration-200 hover:cursor-pointer",
             )}
             alt="thumbnail"
         />
